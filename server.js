cae59575ad8329f1879f7e4aae9cdd66f71d578a
Maycon Do Carmo <Maycongusmao.up1@gmail.com>
2025-06-02 20:18:44 -0300

    fix: corrigindo erro de deploy

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-
 
 const PORT = process.env.PORT || 3000;
 
